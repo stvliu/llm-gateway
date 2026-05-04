91443b2ce55d956514088ace396e66f44d7912dd
Liu Ye <liuye@codingas.com>
2026-05-04 14:04:41 +0800

    fix: import i18n configuration in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import App from './App';
+import './i18n';
 import './styles/global.css';
 
 const queryClient = new QueryClient({
